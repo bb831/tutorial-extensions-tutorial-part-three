eab6f5e7052ea21f51ca20f1bd54cf425529ebda
Clarke <Victor.A.Clarke@medstar.net>
2020-11-05 16:34:19 -0500

    added and edited files

--- js/application.js
+++ js/application.js
@@ -1,50 +1,62 @@
 'use strict';
-  
+ 
 (function () {
-   $(document).ready(function () {
-      // Initialises Tableau Data Extension
-      tableau.extensions.initializeAsync().then(function () {
-      // Once we initialize we call teh drawChartJS function.
+  $(document).ready(function () {
+    tableau.extensions.initializeAsync({ 'configure':configure }).then(function () {
       drawChartJS();
-   }, function () { console.log('Error while Initializing: ' + err.toString()); });
-   });
- 
-   // This javascript function gets data from our worksheet and draws the Doughnut.
-   function drawChartJS() {
-      // Gets all the worksheets in a Tableau Dashboard
-      const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
-      // Finds a worksheet called worksheetData
-      var worksheet = worksheets.find(function (sheet) {
-         return sheet.name === "worksheetData";
+      unregisterSettingsEventListener = tableau.extensions.settings.addEventListener(tableau.TableauEventType.SettingsChanged, (settingsEvent) => {
+        drawChartJS();
       });
+    }, function () { console.log('Error while Initializing: ' + err.toString()); });
+  });
  
-      // Call a function on the worksheet Object to get the Summary Data.
-      worksheet.getSummaryDataAsync().then(function (sumdata) {
-         // Create an empty arrays for our labels and data set.
-         var labels = [];
-         var data = [];
-          
-         // We get our summary data:
-         var worksheetData = sumdata.data;
-         // We loop through our summary data and hardcode which columns goes into Label
-         // and which column goes into the array.
-         for (var i = 0; i < worksheetData.length; i++) {
-            labels.push(worksheetData[i][0].formattedValue);
-            data.push(worksheetData[i][1].value);
-         }
+  function drawChartJS() {
  
-         // Draw the chart as before.
-         var ctx = $("#myChart");
-         var myChart = new Chart(ctx, {
-            type: 'doughnut',
-            data: {
-               labels: labels, // This now comes from Tableau
-               datasets: [{
-                  backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
-                  data: data // This now comes from Tableau
-               }]
-            }
-         });
+    var worksheetName = tableau.extensions.settings.get("worksheet");
+    var categoryColumnNumber = tableau.extensions.settings.get("categoryColumnNumber");
+    var valueColumnNumber = tableau.extensions.settings.get("valueColumnNumber");
+ 
+    const worksheets=tableau.extensions.dashboardContent.dashboard.worksheets;
+    var worksheet=worksheets.find(function (sheet) {
+      return sheet.name===worksheetName;
+    });
+    worksheet.getSummaryDataAsync().then(function (sumdata) {
+      var labels = [];
+      var data = [];
+      var worksheetData = sumdata.data;
+       
+      for (var i=0; i<worksheetData.length; i++) {
+        labels.push(worksheetData[i][categoryColumnNumber-1].formattedValue);
+        data.push(worksheetData[i][valueColumnNumber-1].value);
+      }
+ 
+      var ctx = $("#myChart");
+      var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          labels: labels,
+          datasets: [{
+             backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+             data: data
+          }]
+        }
       });
-   }
+    });
+  }
+ 
+  function configure() {
+    const popupUrl=`${window.location.origin}/dialog.html`;
+    let defaultPayload="";
+    tableau.extensions.ui.displayDialogAsync(popupUrl, defaultPayload, { height:300, width:500 }).then((closePayload) => {
+      drawChartJS();
+    }).catch((error) => {
+      switch (error.errorCode) {
+        case tableau.ErrorCodes.DialogClosedByUser:
+          console.log("Dialog was closed by user");
+          break;
+        default:
+          console.error(error.message);
+      }
+    });
+  }
 })();
